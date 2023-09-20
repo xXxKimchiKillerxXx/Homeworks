be5cb585e8f2066b9bc1dacfd7ff880ac69679dd
xXxKimchiKillerxXx <hakjasu@gmail.com>
2023-09-20 15:44:43 +0900

    Minor Changes

--- Computing Thinking and Programming/Week03/Week03-3.py
+++ Computing Thinking and Programming/Week03/Week03-3.py
@@ -25,15 +25,3 @@ class Magazine(Item):
         self.month = month
     def __str__(self):
         return("제목 : %s, 가격 : %d, 출간 월 : %d" %(self.title, self.price, self.month))
-
-if __name__ == '__main__':
-    book1 = Book('소나기', 10000, 124, '황순원')
-    book2 = Book('메밀꽃 필 무렵', 15000, 142, '이효석')
-    book3 = Book('난쏘공', 12000, 112, '조세희')
-    magazine1 = Magazine('보그', 11000, 9)
-    magazine2 = Magazine('싱글즈', 13000, 8)
-    print('', book1,'\n', book2, '\n', book3, '\n', magazine1, '\n', magazine2)
-    print('총',Item.count,'권')
-    book2.getprice()
-    magazine1.getprice()
-    book1.getprice()
