bbc88aa20e230304483783b55e4480b2d48c3ae8
xXxKimchiKillerxXx <hakjasu@gmail.com>
2023-11-06 17:17:26 +0900

    Small Edit

--- Computational Thinking and Programming/Week07/Week07-3.py
+++ Computational Thinking and Programming/Week07/Week07-3.py
@@ -1,6 +1,6 @@
-def sieve():
-    quote = input().split(' ')
+def sieve(text):
+    quote = text.split(' ')
     filtered = [word[::-1] for word in quote if len(word) >= 5]
     print(filtered)
 
-sieve()
+sieve("This is a more advanced comprehension exercise to practice")
